e53403a2018a566ef45184d41d875d3058c55606
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-01-18 22:00:13 +0000

    add init command

---
+++ cmd/efrc/init.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"efrc/internal/client"
+
+	"github.com/spf13/cobra"
+)
+
+var deviceName string
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize device and generate WireGuard keys",
+	Run: func(cmd *cobra.Command, args []string) {
+		config, err := client.InitConfig(deviceName)
+		if err != nil {
+			log.Fatalf("Failed to initialize: %v", err)
+		}
+
+		fmt.Println("Device initialized!")
+		fmt.Printf("Name:       %s\n", config.DeviceName)
+		fmt.Printf("Public Key: %s\n", config.PublicKey)
+		fmt.Printf("Config:     ~/.efrc/config.json\n")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+	initCmd.Flags().StringVarP(&deviceName, "name", "n", "", "Device name")
+	initCmd.MarkFlagRequired("name")
+}
